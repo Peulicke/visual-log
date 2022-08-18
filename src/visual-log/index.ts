856cc52e6b39f2b5bb2cb005221c2285866bb2f6
Oscar <oscarrpeulicke@gmail.com>
2022-08-18 18:14:08 +0200

    auto scale

--- src/visual-log/index.ts
+++ src/visual-log/index.ts
@@ -1,4 +1,4 @@
-import { Vector3, WebGLRenderer, Scene, PerspectiveCamera, AxesHelper, ArrowHelper, Object3D } from "three";
+import { Vector3, WebGLRenderer, Scene, PerspectiveCamera, AxesHelper, ArrowHelper, Object3D, Box3 } from "three";
 
 const state = { id: "", color: "red" };
 
@@ -16,26 +16,40 @@ type VLog = {
     renderer: WebGLRenderer;
     scene: Scene;
     camera: PerspectiveCamera;
+    axesHelper: AxesHelper;
+    objects: Object3D[];
 };
 
 const vLogs: { [id: string]: VLog } = {};
 
 const createVLog = () => {
+    const epsilon = 1e-10;
     const result: VLog = {
         renderer: new WebGLRenderer(),
         scene: new Scene(),
-        camera: new PerspectiveCamera(75, 1, 0.1, 1000)
+        camera: new PerspectiveCamera(75, 1, 0.1, 1000),
+        axesHelper: new AxesHelper(epsilon),
+        objects: []
     };
-    result.scene.add(new AxesHelper(5));
-    result.camera.position.set(5, 5, 5);
+    result.scene.add(result.axesHelper);
+    result.camera.position.set(epsilon, epsilon, epsilon);
     result.camera.lookAt(0, 0, 0);
     return result;
 };
+
+const aabbMaxSize = ({ min, max }: Box3) => Math.max(...[...min.toArray(), ...max.toArray()].map(v => Math.abs(v)));
+
+const aabbToCamPos = (aabb: Box3) => new Vector3().fromArray([0, 1, 2].map(() => aabbMaxSize(aabb)));
 
 const drawVLog = (id: string) => {
     const vLog = vLogs[id];
     const size = Math.max(window.outerWidth - window.innerWidth - 64, 128);
     vLog.renderer.setSize(size, size);
+    const aabb = vLog.objects.reduce((res, obj) => res.expandByObject(obj), new Box3());
+    vLog.scene.remove(vLog.axesHelper);
+    vLog.axesHelper = new AxesHelper(aabbMaxSize(aabb));
+    vLog.scene.add(vLog.axesHelper);
+    vLog.camera.position.set(...aabbToCamPos(aabb).toArray());
     vLog.renderer.render(vLog.scene, vLog.camera);
     const gl = vLog.renderer.getContext();
     const pixels = new Uint8Array(gl.drawingBufferWidth * gl.drawingBufferHeight * 4);
@@ -60,6 +74,7 @@ export const color = (s: string) => (state.color = s);
 
 const addObj = (obj: Object3D) => {
     if (!vLogs[state.id]) vLogs[state.id] = createVLog();
+    vLogs[state.id].objects.push(obj);
     vLogs[state.id].scene.add(obj);
     shouldRedraw = true;
 };
